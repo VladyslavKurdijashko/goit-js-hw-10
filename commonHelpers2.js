1c095b9be06f11db23c366493dbbeb275361041d
VladyslavKurdijashko <159957674+VladyslavKurdijashko@users.noreply.github.com>
2024-06-30 18:21:35 +0000

    Deploying to gh-pages from @ VladyslavKurdijashko/goit-js-hw-10@f006507e0699bfc8031cf82b4f5143db02e5343b 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{i}from"./assets/vendor-ad859c2f.js";const s=document.querySelector(".form");s.addEventListener("submit",function(o){o.preventDefault();const t=parseInt(s.elements.delay.value),n=s.elements.state.value;new Promise((e,r)=>{setTimeout(()=>{n==="fulfilled"?e(t):r(t)},t)}).then(e=>{i.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{i.error({title:"Error",message:`❌ Rejected promise in ${e}ms`})})});
+//# sourceMappingURL=commonHelpers2.js.map
